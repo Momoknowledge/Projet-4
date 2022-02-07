91f23aa92ddd7fa64851e5b6f885855618544d60
Momo <you@example.com>
2022-02-07 20:32:52 +0100

    Second Test Compress Css

--- compresscss.sh
+++ compresscss.sh
@@ -3,6 +3,6 @@
 
 cat css/bootstrap.css css/et-line.css css/font-awesome.css style.css > bigstyle.css
 purgecss --css bigstyle.css --content ./*.html ./js/bootstrap.js  ./js/jqBootstrapValidation.js  ./js/formHandler.js  ./js/bootstrap.js --output lightstyle.css
-sass lightstyle.css:lightstyle.min.css --style compressed
+sass lightstyle.css:css/lightstyle.min.css --style compressed
 
 rm lightstyle.css bigstyle.css
