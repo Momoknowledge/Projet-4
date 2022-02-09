da88f63df5017374b7bce6649403d0179b468201
Momo <you@example.com>
2022-02-09 19:22:10 +0100

    Update cmd

--- compresscss.sh
+++ compresscss.sh
@@ -1,8 +1,13 @@
 #/bin/bash
 
-
+# Concantène les fichiers css dans bigstyle.css
 cat css/bootstrap.css css/et-line.css css/font-awesome.css style.css > bigstyle.css
+
+# Enlève le css inutilisé, en se basant sur les fichiers "content", de bigstyle
 purgecss --css bigstyle.css --content ./*.html ./js/bootstrap.js  ./js/jqBootstrapValidation.js  ./js/formHandler.js  ./js/bootstrap.js --output lightstyle.css
+
+# minifie le fichier lightstyle.css en dans lightstyle.min.css
 sass lightstyle.css:css/lightstyle.min.css --style compressed
 
+# supprime les fichiers temporaire
 rm lightstyle.css bigstyle.css
