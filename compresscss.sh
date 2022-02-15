4b3e7da6093fb83f5809a48ed6f3d8095560f9a5
Momo <you@example.com>
2022-02-15 20:37:16 +0100

    rajout de la gallery d'image

--- compresscss.sh
+++ compresscss.sh
@@ -4,7 +4,7 @@
 cat css/bootstrap.css css/et-line.css css/font-awesome.css style.css > bigstyle.css
 
 # Enlève le css inutilisé, en se basant sur les fichiers "content", de bigstyle
-purgecss --css bigstyle.css --content ./*.html ./js/bootstrap.js  ./js/jqBootstrapValidation.js  ./js/formHandler.js  ./js/bootstrap.js --output lightstyle.css
+purgecss --css bigstyle.css --content ./*.html ./js/blocs.js ./js/jqBootstrapValidation.js ./js/formHandler.js ./js/bootstrap.js --output lightstyle.css
 
 # minifie le fichier lightstyle.css en dans lightstyle.min.css
 sass lightstyle.css:css/lightstyle.min.css --style compressed
---
+++ compresshtml.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+html-minifier-terser --collapse-whitespace --remove-comments --minify-js true --input-dir ./html --output-dir .
